e886bf7d3ab359e7a8971b8b18196f4dbf1209a9
mitr1x <brb28@ukr.net>
2020-09-04 17:57:32 +0300

    output done

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './compoments/layout/NavBar'
-
 import Dashboard from './compoments/layout/Dashboard';
+import Pokemon from './compoments/pokemons/Pokemon';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.scss';
+
+
+import backroundImage from './pattern.png'
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="App" style={{background: `url(${backroundImage})`}}>
      <NavBar />
      <div className="container">
-       <Dashboard />
+       <Switch>
+         <Route exact path ="/" component={Dashboard} />
+         <Route exact path ="/pokemon/:pokemonIndex" component={Pokemon} />
+       </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
--- pokemon/src/compoments/pokemons/PokemonCard.js
+++ pokemon/src/compoments/pokemons/PokemonCard.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+
 import styled from 'styled-components';
+
+import spinner from '../pokemons/2.gif'
 
 const Sprite = styled.img`
 width:5em;
 height:5em;
-
+display:none;
 `;
 
+const Card = styled.div`
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+&:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+-moz-user-select: none;
+-website-user-select: none;
+user-select: none;
+-o-user-select: none;
+`;
+const StyledLink = styled(Link)`
+text-decoration: none;
+color: black;
+&.focus,
+&:hover,
+&:visited,
+&:link ,
+&:active{
+    text-decoration: none; 
+}
+
+
+`;
 export default class PokemonCard extends Component {
     state={
         name:'',
         imageUrl:'',
-        pokemonIndex: ''
+        pokemonIndex: '',
+        imageLoading: true,
+        toManyRequest: false
     }
     componentDidMount(){
         const {name, url}= this.props;
@@ -31,11 +61,29 @@ export default class PokemonCard extends Component {
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex} </h5>
-                    <Sprite className="card-img-top rounded mx-auto mt-2"
-                    src={this.state.imageUrl}>
-                    </Sprite>
+                    {this.state.imageLoading ?(
+                        <img src={spinner} alt=""
+                        style={{width:'5em', height:'5em'}}
+                        className="card-img-top rounded mx-auto d-block mt-2" />
+                    ) 
+                    : null}
+                    <Sprite 
+                    className="card-img-top rounded mx-auto mt-2"
+                    onLoad={() => this.setState({imageLoading: false})}
+                    onError={() => this.setState({toManyRequest: true})}  
+                    src={this.state.imageUrl}
+                    style={
+                        this.state.toManyRequest ? {display:"none"} : this.state.imageLoading ? null :{display: "block"}
+                    }
+                    />
+                    {this.state.toManyRequest ? (
+                    <h6 className="mx-at">
+                        <span className="badge badge-danger mt-2">To Many Request</span>
+                    </h6>
+                    ) : null}
                        <div className="card-body mx-auto">
                            <h6 className="card-title">
                                {this.state.name
@@ -46,8 +94,9 @@ export default class PokemonCard extends Component {
                                    .join(" ")}
                            </h6>
                        </div>
-                </div>
+                </Card>
+                </StyledLink>
             </div>
-        )
+        );
     }
 }
